a36d200845be045e6315eea4d76d63cd8cd317dd
Belmiro Moreira <moreira.belmiro@gmail.com>
2017-02-28 16:03:53 +0100

    Drop and create target DB

--- dbwheel.py
+++ dbwheel.py
@@ -59,61 +59,91 @@ class DB_query:
 
 class DB_dump:
     def __init__(self, user_source, host_source, port_source, password_source,
-                 name_source, user_target, host_target, port_target,
-                 password_target, name_target, file_path):
+                 name_source, user_target, user_target_admin, host_target, 
+                 port_target, password_target, password_target_admin, name_target, 
+                 file_path):
         self.user_source = " -u " + user_source
         self.host_source = " --host " + host_source
         self.port_source = " --port " + port_source
         self.password_source = " -p" + password_source
         self.name_source = " " + name_source
         self.user_target = " -u " + user_target
+        self.user_target_admin = " -u " + user_target_admin
         self.host_target = " --host " + host_target
         self.port_target = " --port " + port_target
         self.password_target = " -p" + password_target
+        self.password_target_admin = " -p" + password_target_admin
         self.name_target = " " + name_target
         self.file_path = " " + file_path
 
     def download(self):
-        print ("[INFO] download DB")
+        print ("[INFO][%s] download DB" % datetime.datetime.now())
         cmd = "mysqldump --single-transaction" + self.user_source + \
               self.host_source + self.port_source + self.password_source + \
-              self.name_source + " > " + self.file_path
+              self.name_source + " >" + self.file_path
         cmd_out = "mysqldump --single-transaction" + self.user_source + \
               self.host_source + self.port_source + " -pXXX" + \
-              self.name_source + " > " + self.file_path
-        print ("[INFO] CMD: %s" % cmd_out)
+              self.name_source + " >" + self.file_path
+        print ("[INFO][%s] CMD: %s" % (datetime.datetime.now(), cmd_out))
         if os.system(cmd) != 0:
-            print ("[ERROR] Cannot download DB - %s" % self.name_source)
+            print ("[ERROR][%s] Cannot download DB - %s" % (datetime.datetime.now(), self.name_source))
             sys.exit(1)
         else:
             stat = os.stat('db.sql')
-            print ("[INFO] DB file size: %d MB" % (stat.st_size / 1048576))
+            print ("[INFO][%s] DB file size: %d MB" % (datetime.datetime.now(), (stat.st_size / 1048576)))
 
     def upload(self):
-        print ("[INFO] upload DB")
+        print ("[INFO][%s] upload DB" % datetime.datetime.now())
         cmd = "mysql " + self.user_target + self.host_target + \
               self.port_target + self.password_target + \
-              self.name_target + " < " + self.file_path
+              self.name_target + " <" + self.file_path
         cmd_out = "mysql " + self.user_target + self.host_target + \
               self.port_target + " -pXXX" + \
-              self.name_target + " < " + self.file_path
-        print ("[INFO] CMD: %s" % cmd_out)
+              self.name_target + " <" + self.file_path
+        print ("[INFO][%s] CMD: %s" % (datetime.datetime.now(), cmd_out))
         if os.system(cmd) != 0:
             print ("[ERROR] Cannot upload DB")
             sys.exit(1)
 
     def sync(self):
-        print ("[INFO] db sync")
+        print ("[INFO][%s] db sync" % datetime.datetime.now())
         cmd = "nova-manage --config-file nova.conf db sync"
         if os.system(cmd) != 0 :
-            print ("[ERROR] Cannot sync DB")
+            print ("[ERROR][%s] Cannot sync DB" % datetime.datetime.now())
             sys.exit(1)
 
     def version(self):
-        print ("[INFO] db version")
+        print ("[INFO][%s] db version" % datetime.datetime.now())
         cmd = "nova-manage --config-file nova.conf db version"
         out = subprocess.check_output(cmd, shell=True)
         print out
+
+    def drop(self):
+        print ("[INFO][%s] drop target DB" % datetime.datetime.now())
+        cmd = "mysql " + self.user_target_admin + self.host_target + \
+              self.port_target + self.password_target_admin + \
+              " -e \"drop database temp_migration\""
+        cmd_out = "mysql " + self.user_target + self.host_target + \
+              self.port_target + " -pXXX" + \
+              " -e \"drop database temp_migration\""
+        print ("[INFO][%s] CMD: %s" % (datetime.datetime.now(), cmd_out))
+        if os.system(cmd) != 0:
+            print ("[ERROR] Cannot drop target DB")
+            sys.exit(1)
+
+    def create(self):
+        print ("[INFO][%s] create target DB" % datetime.datetime.now())
+        cmd = "mysql " + self.user_target_admin + self.host_target + \
+              self.port_target + self.password_target_admin + \
+              " -e \"create database temp_migration\""
+        cmd_out = "mysql " + self.user_target + self.host_target + \
+              self.port_target + " -pXXX" + \
+              " -e \"create database temp_migration\""
+        print ("[INFO][%s] CMD: %s" % (datetime.datetime.now(), cmd_out))
+        if os.system(cmd) != 0:
+            print ("[ERROR] Cannot create target DB")
+            sys.exit(1)
+
 
 def parse_cmdline_args():
     parser = argparse.ArgumentParser()
@@ -123,7 +153,7 @@ def parse_cmdline_args():
     return parser.parse_args()
 
 def get_target_endpoint(config_file):
-    print ("[INFO] extracting target endpoints")
+    print ("[INFO][%s] extracting target endpoints" % datetime.datetime.now())
     parser = ConfigParser.SafeConfigParser()
     parser.read(config_file)
     endpoints = {}
@@ -133,8 +163,10 @@ def get_target_endpoint(config_file):
         endpoints['user'] = parser.get('target', 'user')
         endpoints['password'] = parser.get('target', 'password')
         endpoints['database'] = parser.get('target', 'database')
+        endpoints['user_admin'] = parser.get('target', 'user_admin')
+        endpoints['password_admin'] = parser.get('target', 'password_admin')
     except :
-        print ("[ERROR] Can't get cell endpoints")
+        print ("[ERROR][%s] Can't get cell endpoints" % datetime.datetime.now())
         sys.exit(1)
     return endpoints
 
@@ -145,7 +177,7 @@ def get_cells(config_file):
     return cells
 
 def get_cell_endpoint(cell, config_file):
-    print ("[INFO] extracting cell endpoints - %s" % cell)
+    print ("[INFO][%s] extracting cell endpoints - %s" % (datetime.datetime.now(), cell))
     parser = ConfigParser.SafeConfigParser()
     parser.read(config_file)
     endpoints = {}
@@ -156,7 +188,7 @@ def get_cell_endpoint(cell, config_file):
         endpoints['password'] = parser.get(cell, 'password')
         endpoints['database'] = parser.get(cell, 'database')
     except:
-        print ("[ERROR] Can't get cell endpoints - %s" % cell)
+        print ("[ERROR][%s] Can't get cell endpoints - %s" % (datetime.datetime.now(), cell))
         sys.exit(1)
     return endpoints
 
@@ -191,24 +223,24 @@ def main():
                           source_endpoint['password'],
                           source_endpoint['database'],
                           target_endpoint['user'],
+                          target_endpoint['user_admin'],
                           target_endpoint['host'],
                           target_endpoint['port'],
                           target_endpoint['password'],
+                          target_endpoint['password_admin'],
                           target_endpoint['database'],
                           'db.sql'
                          )
 
-        print ("[INFO] %s" % datetime.datetime.now())
+        db_dump.drop()
+        db_dump.create()
         db_dump.download()
-        print ("[INFO] %s" % datetime.datetime.now())
         db_dump.upload()
-        print ("[INFO] %s" % datetime.datetime.now())
         db_dump.version()
-        print ("[INFO] %s" % datetime.datetime.now())
         db_dump.sync()
-        print ("[INFO] %s" % datetime.datetime.now())
         db_dump.version()
         print "\n---\n"
 
 if __name__ == "__main__":
     main()
+
